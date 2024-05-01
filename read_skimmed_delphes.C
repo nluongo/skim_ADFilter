3a7d0d1a263b55a61f4846d8fa6dfba427bc39a6
Wasikul Islam <wasikul.islam@cern.ch>
2024-05-01 13:37:41 -0700

    Add files via upload

---
+++ read_skimmed_delphes.C
@@ -0,0 +1,91 @@
+#include <iostream>
+#include "TFile.h"
+#include "TTree.h"
+#include "TClonesArray.h"
+#include "TLorentzVector.h"
+
+void readRootFile(const char* filename) {
+    // Open the root file
+    TFile inputFile(filename, "READ");
+    if (!inputFile.IsOpen()) {
+        std::cerr << "Error: Could not open file " << filename << std::endl;
+        return;
+    }
+
+    // Get the TTree from the root file
+    TTree* ntuple = dynamic_cast<TTree*>(inputFile.Get("Ntuple"));
+    if (!ntuple) {
+        std::cerr << "Error: Could not find TTree 'Ntuple' in file " << filename << std::endl;
+        return;
+    }
+
+    // Create TClonesArrays to store TLorentzVectors
+    TClonesArray* jetArray = nullptr;
+    TClonesArray* bjetArray = nullptr;
+    TClonesArray* electronArray = nullptr;
+    TClonesArray* muonArray = nullptr;
+    TClonesArray* photonArray = nullptr;
+
+    // Set branch addresses for each TClonesArray
+    ntuple->SetBranchAddress("Jets", &jetArray);
+    ntuple->SetBranchAddress("BJets", &bjetArray);
+    ntuple->SetBranchAddress("Electrons", &electronArray);
+    ntuple->SetBranchAddress("Muons", &muonArray);
+    ntuple->SetBranchAddress("Photons", &photonArray);
+
+    // Loop over all entries in the TTree
+    Long64_t numEntries = ntuple->GetEntries();
+    for (Long64_t i = 0; i < numEntries; ++i) {
+        ntuple->GetEntry(i);
+
+        std::cout << "Event " << i << ":" << std::endl;
+
+        // Print TLorentzVectors for jets
+        std::cout << " Jets:" << std::endl;
+        for (int j = 0; j < jetArray->GetEntries(); ++j) {
+            TLorentzVector* jet = dynamic_cast<TLorentzVector*>(jetArray->At(j));
+            std::cout << "   Jet " << j << ": (Pt, Eta, Phi, M) = (" << jet->Pt() << ", " << jet->Eta() << ", " << jet->Phi() << ", " << jet->M() << ")" << std::endl;
+        }
+
+        // Print TLorentzVectors for b-jets
+        std::cout << "  B-Jets:" << std::endl;
+        for (int j = 0; j < bjetArray->GetEntries(); ++j) {
+            TLorentzVector* bjet = dynamic_cast<TLorentzVector*>(bjetArray->At(j));
+            std::cout << "   B-Jet " << j << ": (Pt, Eta, Phi, M) = (" << bjet->Pt() << ", " << bjet->Eta() << ", " << bjet->Phi() << ", " << bjet->M() << ")" << std::endl;
+        }
+
+        // Print TLorentzVectors for electrons
+        std::cout << " Electrons:" << std::endl;
+        for (int j = 0; j < electronArray->GetEntries(); ++j) {
+            TLorentzVector* electron = dynamic_cast<TLorentzVector*>(electronArray->At(j));
+            std::cout << "   Electron " << j << ": (Pt, Eta, Phi, M) = (" << electron->Pt() << ", " << electron->Eta() << ", " << electron->Phi() << ", " << electron->M() << ")" << std::endl;
+        }
+
+        // Print TLorentzVectors for muons
+        std::cout << " Muons:" << std::endl;
+        for (int j = 0; j < muonArray->GetEntries(); ++j) {
+            TLorentzVector* muon = dynamic_cast<TLorentzVector*>(muonArray->At(j));
+            std::cout << "   Muon " << j << ": (Pt, Eta, Phi, M) = (" << muon->Pt() << ", " << muon->Eta() << ", " << muon->Phi() << ", " << muon->M() << ")" << std::endl;
+        }
+
+        // Print TLorentzVectors for photons
+        std::cout << " Photons:" << std::endl;
+        for (int j = 0; j < photonArray->GetEntries(); ++j) {
+            TLorentzVector* photon = dynamic_cast<TLorentzVector*>(photonArray->At(j));
+            std::cout << "   Photon " << j << ": (Pt, Eta, Phi, M) = (" << photon->Pt() << ", " << photon->Eta() << ", " << photon->Phi() << ", " << photon->M() << ")" << std::endl;
+        }
+
+        std::cout << std::endl;
+    }
+
+    // Close the input file
+    inputFile.Close();
+}
+
+int read_skimmed_delphes() {
+    // Call the function to read the root file and print TLorentzVectors
+    readRootFile("skimmed_delphes.root");
+
+    return 0;
+}
+
---
+++ skim_Delphes.c
@@ -0,0 +1,331 @@
+#include <TFile.h>
+#include <TTree.h>
+#include <vector>
+#include <iostream>
+#include <TLeaf.h>
+
+void readEventData(const char* inputFileName, const char* outputFileName) {
+    // Open the ROOT file
+    TFile inputFile(inputFileName);
+    if (inputFile.IsZombie()) {
+        std::cerr << "Error: Unable to open file " << inputFileName << std::endl;
+        return;
+    }
+
+    // Access the Delphes TTree
+    TTree* tree = dynamic_cast<TTree*>(inputFile.Get("Delphes"));
+    if (!tree) {
+        std::cerr << "Error: Unable to access tree Delphes in file " << inputFileName << std::endl;
+        return;
+    }
+    
+    // Find values for kMaxJet,kMaxElectron, kMaxMuon, kMaxPhoton
+    Int_t kMaxJet = 0;
+    Int_t kMaxElectron = 0;
+    Int_t kMaxPhoton = 0;
+    Int_t kMaxMuon = 0;
+
+    TBranch* jet_Pt_Branch = tree->GetBranch("Jet.PT");
+    TBranch* el_Pt_Branch = tree->GetBranch("Electron.PT");
+    TBranch* mu_Pt_Branch = tree->GetBranch("Muon.PT");
+    TBranch* ph_Pt_Branch = tree->GetBranch("Photon.PT");
+
+    TLeaf* jet_leaf = jet_Pt_Branch->GetLeaf("Jet.PT");
+    TLeaf* el_leaf = el_Pt_Branch->GetLeaf("Electron.PT");
+    TLeaf* mu_leaf = mu_Pt_Branch->GetLeaf("Muon.PT");
+    TLeaf* ph_leaf = ph_Pt_Branch->GetLeaf("Photon.PT");
+
+    Long64_t numEntries1 = tree->GetEntries();
+    for (Long64_t i = 0; i < numEntries1; ++i) {
+        jet_Pt_Branch->GetEntry(i);
+        Int_t len_jet = jet_leaf->GetLen();
+        if (len_jet > kMaxJet) {
+            kMaxJet = len_jet;
+        }
+    }
+    
+    for (Long64_t i = 0; i < numEntries1; ++i) {
+        el_Pt_Branch->GetEntry(i);
+        Int_t len_el = el_leaf->GetLen();
+        if (len_el > kMaxElectron) {
+            kMaxElectron = len_el;
+        }
+    }
+      
+    for (Long64_t i = 0; i < numEntries1; ++i) {
+        mu_Pt_Branch->GetEntry(i);
+        Int_t len_mu = mu_leaf->GetLen();
+        if (len_mu > kMaxMuon) {
+            kMaxMuon = len_mu;
+        }
+    }
+    
+    for (Long64_t i = 0; i < numEntries1; ++i) {
+        ph_Pt_Branch->GetEntry(i);
+        Int_t len_ph = ph_leaf->GetLen();
+        if (len_ph > kMaxPhoton) {
+            kMaxPhoton = len_ph;
+        }
+    }
+    std::cout << "kMaxJet: " << kMaxJet << std::endl;
+    std::cout << "kMaxElectron: " << kMaxElectron << std::endl;
+    std::cout << "kMaxMuon: " << kMaxMuon << std::endl;
+    std::cout << "kMaxPhoton: " << kMaxPhoton << std::endl;
+    
+    
+    Float_t         Jet_PT[kMaxJet];   //[Jet_]
+    Float_t         Jet_Eta[kMaxJet];   //[Jet_]
+    Float_t         Jet_Phi[kMaxJet];   //[Jet_]
+    Float_t         Jet_Mass[kMaxJet];   //[Jet_]
+    UInt_t          Jet_BTag[kMaxJet];   //[Jet_]
+
+    Float_t         Electron_PT[kMaxElectron];   //[Electron_]
+    Float_t         Electron_Eta[kMaxElectron];   //[Electron_]
+    Float_t         Electron_Phi[kMaxElectron];   //[Electron_]
+    //Float_t         Electron_Mass[kMaxElectron];   //[Electron_]
+
+    Float_t         Muon_PT[kMaxMuon];   //[Muon_]
+    Float_t         Muon_Eta[kMaxMuon];   //[Muon_]
+    Float_t         Muon_Phi[kMaxMuon];   //[Muon_]
+    //Float_t         Muon_Mass[kMaxMuon];   //[Muon_]
+
+    Float_t         Photon_PT[kMaxPhoton];   //[Photon_]
+    Float_t         Photon_Eta[kMaxPhoton];   //[Photon_]
+    Float_t         Photon_Phi[kMaxPhoton];   //[Photon_]
+    Float_t         Photon_E[kMaxPhoton];   //[Photon_]
+    
+    TBranch        *b_Jet_PT;   //!
+    TBranch        *b_Jet_Eta;   //!
+    TBranch        *b_Jet_Phi;   //!
+    TBranch        *b_Jet_T;   //!
+    TBranch        *b_Jet_Mass;   //!
+    TBranch        *b_Jet_BTag;   //!
+    TBranch        *b_Electron_PT;   //!
+    TBranch        *b_Electron_Eta;   //!
+    TBranch        *b_Electron_Phi;   //!
+    //TBranch        *b_Electron_Mass;   //!
+    TBranch        *b_Muon_PT;   //!
+    TBranch        *b_Muon_Eta;   //!
+    TBranch        *b_Muon_Phi;   //!
+    //TBranch        *b_Muon_Mass;   //!
+    TBranch        *b_Photon_PT;   //!
+    TBranch        *b_Photon_Eta;   //!
+    TBranch        *b_Photon_Phi;   //!
+    TBranch        *b_Photon_E;   //!
+    
+    tree->SetBranchAddress("Jet.PT", Jet_PT, &b_Jet_PT);
+    tree->SetBranchAddress("Jet.Eta", Jet_Eta, &b_Jet_Eta);
+    tree->SetBranchAddress("Jet.Phi", Jet_Phi, &b_Jet_Phi);
+    tree->SetBranchAddress("Jet.Mass", Jet_Mass, &b_Jet_Mass);
+    tree->SetBranchAddress("Jet.BTag", Jet_BTag, &b_Jet_BTag);
+    
+    tree->SetBranchAddress("Electron.PT", Electron_PT, &b_Electron_PT);
+    tree->SetBranchAddress("Electron.Eta", Electron_Eta, &b_Electron_Eta);
+    tree->SetBranchAddress("Electron.Phi", Electron_Phi, &b_Electron_Phi);
+    //tree->SetBranchAddress("Electron.Mass", Electron_Mass, &b_Electron_Mass);
+
+    tree->SetBranchAddress("Muon.PT", Muon_PT, &b_Muon_PT);
+    tree->SetBranchAddress("Muon.Eta", Muon_Eta, &b_Muon_Eta);
+    tree->SetBranchAddress("Muon.Phi", Muon_Phi, &b_Muon_Phi);
+    //tree->SetBranchAddress("Muon.Mass", Muon_Mass, &b_Muon_Mass);
+
+    tree->SetBranchAddress("Photon.PT", Photon_PT, &b_Photon_PT);
+    tree->SetBranchAddress("Photon.Eta", Photon_Eta, &b_Photon_Eta);
+    tree->SetBranchAddress("Photon.Phi", Photon_Phi, &b_Photon_Phi);
+    tree->SetBranchAddress("Photon.E", Photon_E, &b_Photon_E);
+    
+    // Loop over entries in the TTree and create Vectors
+    std::vector<std::vector<TLorentzVector>> jet_Vectors;
+    std::vector<std::vector<TLorentzVector>> bjet_Vectors;
+    std::vector<std::vector<TLorentzVector>> el_Vectors;
+    std::vector<std::vector<TLorentzVector>> mu_Vectors;
+    std::vector<std::vector<TLorentzVector>> photon_Vectors;
+
+    Long64_t numEntries = tree->GetEntries();
+    cout << "no. of events :" << numEntries << endl;
+    for (Long64_t i = 0; i < numEntries; ++i) {
+        // Clear the vector for the current event
+        //jet_Vectors.clear();
+        std::vector<TLorentzVector> event_JetVectors;
+        std::vector<TLorentzVector> event_bJetVectors;
+        std::vector<TLorentzVector> event_elVectors;
+        std::vector<TLorentzVector> event_muVectors;
+        std::vector<TLorentzVector> event_phVectors;
+
+        // Get entry i
+        tree->GetEntry(i);
+        cout << "Event " << i << endl; 
+
+        for (Int_t j = 0; j < kMaxJet; ++j) {
+            TLorentzVector jet_Vector;
+            jet_Vector.SetPtEtaPhiM(Jet_PT[j], Jet_Eta[j], Jet_Phi[j], Jet_Mass[j]);
+	    event_JetVectors.push_back(jet_Vector);
+        }
+        jet_Vectors.push_back(event_JetVectors);
+
+        
+        for (Int_t j = 0; j < kMaxJet; ++j) {
+            TLorentzVector bjet_Vector;
+            //cout << j << " isbTag :" << Jet_BTag[j] << endl;
+            if (Jet_BTag[j]==1){
+                bjet_Vector.SetPtEtaPhiM(Jet_PT[j], Jet_Eta[j], Jet_Phi[j], Jet_Mass[j]);
+                event_bJetVectors.push_back(bjet_Vector);
+            }
+        }
+        bjet_Vectors.push_back(event_bJetVectors);
+        
+        // If Electron_Mass is not present in file, we can set it 0.000511
+        //Float_t Electron_Mass[kMaxElectron];
+        //for (Int_t j = 0; j < kMaxElectron; ++j) {
+        //    Electron_Mass[j] = 0.000511;
+        //}
+
+        for (Int_t j = 0; j < kMaxElectron; ++j) {
+            TLorentzVector electronVector;
+            electronVector.SetPtEtaPhiM(Electron_PT[j], Electron_Eta[j], Electron_Phi[j],  0.000511 );
+            event_elVectors.push_back(electronVector);
+        }
+        el_Vectors.push_back(event_elVectors);
+
+        
+        // If Muon_Mass is not present in file, we can set it 0.1057
+        //Float_t Muon_Mass[kMaxMuon];
+        //for (Int_t j = 0; j < kMaxMuon; ++j) {
+        //    Muon_Mass[j] = 0.1057;
+        //}
+
+        for (Int_t j = 0; j < kMaxMuon; ++j) {
+            TLorentzVector muonVector;
+            muonVector.SetPtEtaPhiM(Muon_PT[j], Muon_Eta[j], Muon_Phi[j], 0.1057 );
+            event_muVectors.push_back(muonVector);
+        }
+        mu_Vectors.push_back(event_muVectors);
+
+
+          // Create TLorentzVector objects for photons and store them in the vector
+        for (Int_t j = 0; j < kMaxPhoton; ++j) {
+            TLorentzVector photonVector;
+            photonVector.SetPtEtaPhiM(Photon_PT[j], Photon_Eta[j], Photon_Phi[j], 0.0);
+            event_phVectors.push_back(photonVector);
+        }
+        photon_Vectors.push_back(event_phVectors);
+
+    }
+    // Close the file
+    inputFile.Close();
+    
+    TFile outputFile(outputFileName, "RECREATE");
+    TTree ntuple("Ntuple", "Ntuple containing TLorentzVector objects");
+
+    // Create TClonesArrays to store TLorentzVectors for each object type
+    TClonesArray jetArray("TLorentzVector", 20);
+    TClonesArray bjetArray("TLorentzVector", 20);
+    TClonesArray electronArray("TLorentzVector", 20);
+    TClonesArray muonArray("TLorentzVector", 20);
+    TClonesArray photonArray("TLorentzVector", 20);
+
+    // Add branches to the TTree for each TClonesArray
+    ntuple.Branch("Jets", &jetArray, 256000, 0);
+    ntuple.Branch("BJets", &bjetArray, 256000, 0);
+    ntuple.Branch("Electrons", &electronArray, 256000, 0);
+    ntuple.Branch("Muons", &muonArray, 256000, 0);
+    ntuple.Branch("Photons", &photonArray, 256000, 0);
+    
+    // Loop over all events and fill TLorentzVectors into TClonesArrays
+    for (size_t i = 0; i < jet_Vectors.size(); ++i) {
+        // Clear the TClonesArrays before filling them for the current event
+        jetArray.Clear();
+        bjetArray.Clear();
+        electronArray.Clear();
+        muonArray.Clear();
+        photonArray.Clear();
+        
+        // Fill TClonesArrays with TLorentzVectors for jets
+        for (const auto& jet : jet_Vectors[i]) {
+            new (jetArray[jetArray.GetEntries()]) TLorentzVector(jet);
+        }
+
+        // Fill TClonesArrays with TLorentzVectors for b-jets
+        for (const auto& bjet : bjet_Vectors[i]) {
+            new (bjetArray[bjetArray.GetEntries()]) TLorentzVector(bjet);
+        }
+
+        // Fill TClonesArrays with TLorentzVectors for electrons
+        for (const auto& electron : el_Vectors[i]) {
+            new (electronArray[electronArray.GetEntries()]) TLorentzVector(electron);
+        }
+
+        // Fill TClonesArrays with TLorentzVectors for muons
+        for (const auto& muon : mu_Vectors[i]) {
+            new (muonArray[muonArray.GetEntries()]) TLorentzVector(muon);
+        }
+
+        // Fill TClonesArrays with TLorentzVectors for photons
+        for (const auto& photon : photon_Vectors[i]) {
+            new (photonArray[photonArray.GetEntries()]) TLorentzVector(photon);
+        }
+
+        // Fill the TTree for the current event
+        ntuple.Fill();
+    }
+
+    // Write the TTree to the output file
+    ntuple.Write();
+
+    // Close the output file
+    outputFile.Close();
+    
+    // Read the vectors
+    cout << "jet_Vectors.size :" << jet_Vectors.size() << endl;
+    cout << "bjet_Vectors.size :" << bjet_Vectors.size() << endl;
+    cout << "el_Vectors.size :" << el_Vectors.size() << endl;
+    cout << "mu_Vectors.size :" << mu_Vectors.size() << endl;
+    cout << "photon_Vectors.size :" << photon_Vectors.size() << endl;
+ 
+    
+    /*
+    //Read vectors
+    // Loop over all events in jet_Vectors
+    for (size_t i = 0; i < jet_Vectors.size(); ++i) {
+        // Access the vector of TLorentzVectors for the current event
+        const std::vector<TLorentzVector>& event_Jets = jet_Vectors[i];
+        const std::vector<TLorentzVector>& event_bJets = bjet_Vectors[i];
+        const std::vector<TLorentzVector>& event_el = el_Vectors[i];
+        const std::vector<TLorentzVector>& event_mu = mu_Vectors[i];
+        const std::vector<TLorentzVector>& event_ph = photon_Vectors[i];
+
+        // Print the number of jets in the current event
+        cout << "For event# " << i << " #jets: " << event_Jets.size() << " #bjets: " << event_bJets.size() << " #electrons: " << event_el.size() << " #muons: " << event_mu.size() << " #photons: " << event_ph.size() << endl;
+        
+        // Check if there are any jets in the current event
+        if (!event_Jets.empty()) {
+            // Loop over all jets in the current event
+            for (size_t j = 0; j < event_Jets.size(); ++j) {
+                // Access the j-th TLorentzVector in the current event
+                const TLorentzVector& jet = event_Jets[j];
+                
+                // Print the values of the j-th jet in the current event
+                Float_t pt = jet.Pt();
+                Float_t eta = jet.Eta();
+                Float_t phi = jet.Phi();
+                Float_t mass = jet.M();
+                
+                //cout << "Event " << i << ", Jet " << j << ": PT=" << pt << ", Eta=" << eta << ", Phi=" << phi << ", Mass=" << mass << endl;
+            }
+        } else {
+            cout << "Event " << i << ": No jets." << endl;
+        }
+    }
+    */
+    
+    
+}
+
+int skim_Delphes() {
+    const char* inputFileName = "Hplus_1800GeV_SLHA2.root"; //mg5_pythia_ttbarW_14tev_1.root";
+    const char* outputFileName = "skimmed_delphes.root";
+    readEventData(inputFileName, outputFileName);
+
+    return 0;
+}
+
